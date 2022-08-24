cb4802d6ce9574161b13a19c64a160bfafd08ebf
Diego Ramos <diegorramos84@gmail.com>
2022-08-24 14:37:16 +0100

    add heroku

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ app.post('/api/persons' , (request, response) => {
 
 })
 
-
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
